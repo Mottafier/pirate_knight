eea86528453587e00f81d53b14d25c8d74efadfb
Mottafier <djm831@gmail.com>
2024-03-17 16:28:38 -0400

    stuff

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,8 @@
-function sendMail(){
+//import '../node_modules/dotenv/config.js'
+//import { MailerSend, EmailParams, Sender, Recipient } from "../node_modules/mailersend";
+
+async function sendMail(){
+
     //var input = document.getElementById('message_input')
     var input = $('#message_input')//document.getElementById('message_input')
     //Check if the input box is empty
@@ -13,6 +17,53 @@ function sendMail(){
         //Incorporating email sending is too complex at the moment, so I have replace it with a console log for now
         console.log("Sending message '" + input.val() + "' to Pirate_Knight@gmail.com")
         input.val('')
+
+        /*
+        const mailerSend = new MailerSend({
+            apiKey: "mlsn.ae977b55651912708a27358d2709221790ea6f1fe22bb7e1ca90f2058ac82b64",
+          });
+        const sentFrom = new Sender("trial-yzkq340n6v6gd796.mlsender.net", "Customer");
+        const recipients = [
+            new Recipient("djm831@gmail.com", "Pirate Knight Team")
+          ];
+
+        const emailParams = new EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setReplyTo(sentFrom)
+        .setSubject("This is a Subject")
+        .setHtml("<strong>This is the HTML content</strong>")
+        .setText("This is the text content");
+
+        await mailerSend.email.send(emailParams);
+        */
+
+        var msgJSON = {
+            "from": {
+              "email": "MS_YTbbrW@trial-yzkq340n6v6gd796.mlsender.net",
+              "name": "MailerSend"
+            },
+            "to": [
+              {
+                "email": "djm831@gmail.com",
+                "name": "John Mailer"
+              }
+            ],
+            "subject": "Hello from !",
+            "text": "This is just a friendly hello from your friends at .",
+            "html": "<b>This is just a friendly hello from your friends at.</b>"
+          }
+
+        const response = await fetch("https://api.mailersend.com/v1/email", {
+            method: "POST",
+            body: JSON.stringify(msgJSON),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": "Bearer mlsn.e0a793eeee35496fbe30f539bbe1439687580152221d44450d36d924edebd00d"
+            }
+        });
+
+        console.log(response)
         
         //hide input elements
         //let input_elements = document.getElementsByClassName('text_input');
